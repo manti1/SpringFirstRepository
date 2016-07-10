602e295c083a2c055ea0b17afa0234f4099a0334
Kumar <Kumar@sanjeev-pc>
2016-07-11 03:30:15 +0530

    initial

---
+++ SpringRestDemo/src/main/java/hello/Application.java
@@ -0,0 +1,9 @@
+package main.java.hello;
+
+//@SpringBootApplication
+public class Application {
+
+    /*public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }*/
+}
